fa24cf093c59ea2305f5639da17bdc0dffd76a53
slydragonn <alogo18m@gmail.com>
2023-02-07 20:10:46 -0500

    feat(plugins): add background transparent

--- .config/nvim/lua/slydragonn/plugins.lua
+++ .config/nvim/lua/slydragonn/plugins.lua
@@ -148,4 +148,23 @@ return require("packer").startup(function(use)
 			require("slydragonn.configs.autopairs")
 		end,
 	})
+
+	-- Background Transparent
+	use({
+		"xiyaowong/nvim-transparent",
+		config = function()
+			require("transparent").setup({
+				enable = true,
+				extra_groups = {
+					"BufferLineTabClose",
+					"BufferlineBufferSelected",
+					"BufferLineFill",
+					"BufferLineBackground",
+					"BufferLineSeparator",
+					"BufferLineIndicatorSelected",
+				},
+				exclude = {},
+			})
+		end,
+	})
 end)
