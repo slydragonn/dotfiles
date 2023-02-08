fd3c51714959562b60e8a991ef31c9f77a089dc1
slydragonn <alogo18m@gmail.com>
2023-02-07 20:05:39 -0500

    fix(plugins): add colorizer config

--- .config/nvim/lua/slydragonn/plugins.lua
+++ .config/nvim/lua/slydragonn/plugins.lua
@@ -109,7 +109,12 @@ return require("packer").startup(function(use)
 	})
 
 	-- Show colors
-	use("norcalli/nvim-colorizer.lua")
+	use({
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup({ "*" })
+		end,
+	})
 
 	-- Terminal
 	use({
